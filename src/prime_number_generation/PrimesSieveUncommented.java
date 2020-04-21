1250ff516cdc7ba86929a98bf641262bf2653ab6
adi forkasiewicz <adiforka@gmail.com>
2020-04-21 15:28:53 +0200

    added uncommented (uncluttered) sieve algo

---
+++ src/prime_number_generation/PrimesSieveUncommented.java
@@ -0,0 +1,36 @@
+package prime_number_generation;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PrimesSieveUncommented {
+    public static void main(String[] args) {
+        PrimesSieveUncommented p = new PrimesSieveUncommented();
+        List<Integer> primes = p.getPrimesWithSieveOfEratosthenes(300);
+        System.out.println(primes);
+
+    }
+
+    public List<Integer> getPrimesWithSieveOfEratosthenes(int maxNoChecked) {
+
+        boolean[] primeChecks = new boolean[maxNoChecked + 1];
+
+        Arrays.fill(primeChecks, true);
+
+        for (int p = 2; p * p <= maxNoChecked; p++) {
+            if (primeChecks[p]) {
+                for (int i = p * 2; i <= maxNoChecked; i += p) {
+                    primeChecks[i] = false;
+                }
+            }
+        }
+
+        List<Integer> primes = new LinkedList<>();
+        for (int i = 2; i <= maxNoChecked; i++) {
+            if (primeChecks[i])
+                primes.add(i);
+        }
+        return primes;
+    }
+}
